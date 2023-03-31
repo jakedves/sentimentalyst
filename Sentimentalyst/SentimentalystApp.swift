2b5ae865b05f3430407ab2a2bbcef9a67172fe1e
Jake <jakedves@gmail.com>
2023-04-01 00:30:19 +0100

    Initial Commit

---
+++ Sentimentalyst/SentimentalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentalystApp.swift
+//  Sentimentalyst
+//
+//  Created by Jake Davies on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SentimentalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
