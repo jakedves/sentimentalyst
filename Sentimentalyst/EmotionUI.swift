e06d4ac4fc33ac4dcfdd46f8bac981690784edb4
Jake <jakedves@gmail.com>
2023-04-01 22:11:21 +0100

    Added sentiment -> color map

--- Sentimentalyst/EmotionUI.swift
+++ Sentimentalyst/EmotionUI.swift
@@ -15,3 +15,24 @@ let EmotionColor: [Emotion : Color] = [
     .love : .pink,
     .unknown : .orange
 ]
+
+// 5 categories: awful, bad, neutral, good, amazing
+// possible values of sentiment: -1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1
+// 11 values => 2 values per category, 3 for good
+func sentimentColor(_ sentiment: Sentiment) -> Color {
+    if sentiment < -0.6 { // -1, -0.8
+        return .red
+    } else if sentiment < -0.2 { // -0.6, -0.4
+        return .orange
+    } else if sentiment < 0.2 { // -0.2, 0
+        return .yellow
+    } else if sentiment < 0.7 { // -0.2, 0.4, 0.6
+        return .green.opacity(0.6)
+    } else {
+        return .green
+    }
+}
+
+func sentimentLabel(_ sentiment: Sentiment) -> String {
+    return ""
+}
