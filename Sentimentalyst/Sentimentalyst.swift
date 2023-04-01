3cf21fbff771710809505659b0fe57c9b4b1dfb9
Jake <jakedves@gmail.com>
2023-04-01 01:08:05 +0100

    rename app struct

---
+++ Sentimentalyst/Sentimentalyst.swift
@@ -0,0 +1,17 @@
+//
+//  Sentimentalyst.swift
+//  Sentimentalyst
+//
+//  Created by Jake Davies on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Sentimentalyst: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScreen()
+        }
+    }
+}
