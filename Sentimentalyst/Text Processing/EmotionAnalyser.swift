8d3ac86a1b7bc419e78da0691ab9c220769809ca
Jake <jakedves@gmail.com>
2023-04-01 17:13:51 +0100

    fill in implementation

--- Sentimentalyst/Text Processing/EmotionAnalyser.swift
+++ Sentimentalyst/Text Processing/EmotionAnalyser.swift
@@ -5,24 +5,44 @@
 //  Created by Jake Davies on 01/04/2023.
 //
 
-import Foundation
+import CoreML
+import NaturalLanguage
 
 struct EmotionAnalyser: TextAnalyser {
     private let destructurer: TextDestructurer
+    private let emotionClassifier: NLModel
     
     var labelPerSentence: [Emotion] {
-        []
+        emotions(from: destructurer.sentences)
     }
     
     var labelPerParagraph: [Emotion] {
-        []
+        emotions(from: destructurer.paragraphs)
     }
     
-    var labelOverall: [Emotion] {
-        []
+    var labelOverall: Emotion {
+        emotion(for: destructurer.text)
     }
     
-    init(_ text: String) {
+    // failable init => useless without the model
+    init?(_ text: String) {
         self.destructurer = TextDestructurer(text)
+        
+        do {
+            let config = MLModelConfiguration()
+            let ml = try TextClassifier(configuration: config).model
+            self.emotionClassifier = try NLModel(mlModel: ml)
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func emotions(from list: [String]) -> [Emotion] {
+        return list.map(emotion)
+    }
+    
+    private func emotion(for value: String) -> Emotion {
+        Emotion(rawValue: emotionClassifier.predictedLabel(for: value) ?? "unknown") ?? .unknown
     }
 }
