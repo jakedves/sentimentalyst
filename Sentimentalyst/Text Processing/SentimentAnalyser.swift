aa28e7240f670a0526e6618674958460d991f626
Jake <jakedves@gmail.com>
2023-04-01 17:29:08 +0100

    sentiment analyser implementation

--- Sentimentalyst/Text Processing/SentimentAnalyser.swift
+++ Sentimentalyst/Text Processing/SentimentAnalyser.swift
@@ -5,26 +5,32 @@
 //  Created by Jake Davies on 01/04/2023.
 //
 
-import Foundation
+import NaturalLanguage
 
 struct SentimentAnalyser: TextAnalyser {
-    typealias Sentiment = Float
+    typealias Sentiment = Double
     private let destructurer: TextDestructurer
     
-    
     var labelPerSentence: [Sentiment] {
-        []
+        destructurer.sentences.map(sentiment)
     }
     
     var labelPerParagraph: [Sentiment] {
-        []
+        destructurer.paragraphs.map(sentiment)
     }
     
-    var labelOverall: [Sentiment] {
-        []
+    var labelOverall: Sentiment {
+        sentiment(for: destructurer.text)
     }
     
     init(_ text: String) {
         self.destructurer = TextDestructurer(text)
+    }
+    
+    func sentiment(for text: String) -> Double {
+        let tagger = NLTagger(tagSchemes: [.sentimentScore])
+        tagger.string = text
+        let (sentiment, _) = tagger.tag(at: text.startIndex, unit: .document, scheme: .sentimentScore)
+        return Double(sentiment?.rawValue ?? "0") ?? 0
     }
 }
