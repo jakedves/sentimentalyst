5cdddc333c855b753ef5f96af5fd1d91100d58c3
Jake <jakedves@gmail.com>
2023-04-01 16:58:46 +0100

    create a map from emotions to colours

---
+++ Sentimentalyst/EmotionUI.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionUI.swift
+//  Sentimentalyst
+//
+//  Created by Jake Davies on 01/04/2023.
+//
+
+import SwiftUI
+
+let EmotionColor: [Emotion : Color] = [
+    .joy : .green,
+    .anger : .red,
+    .sadness : .blue,
+    .fear : .gray,
+    .love : .pink,
+    .unknown : .orange
+]
