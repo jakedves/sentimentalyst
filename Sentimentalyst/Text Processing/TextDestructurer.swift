e17b090c37570ecaa30d5d75d7725ef60edf7eb5
Jake <jakedves@gmail.com>
2023-04-03 02:17:09 +0100

    remove debug string

--- Sentimentalyst/Text Processing/TextDestructurer.swift
+++ Sentimentalyst/Text Processing/TextDestructurer.swift
@@ -25,7 +25,6 @@ struct TextDestructurer {
     
     init(_ text: String) {
         self.text = text
-        print(text + "\n\n")
         let wordTokenizer = NLTokenizer(unit: .word)
         let sentenceTokenizer = NLTokenizer(unit: .sentence)
         let paragraphTokenizer = NLTokenizer(unit: .paragraph)
